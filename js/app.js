81ef004f68ab5441860c9a47bbf21ac1a8e51e29
zenmai25 <arvindespi@gmail.com>
2023-08-11 15:17:29 +0800

    activity_multiplication_table
    activity

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('Multiplication Table')
+
+let i = 1;
+
+while (i <= 10) {
+  console.log("10 * " + i + " = " + (10 * i));
+  i++; 
+}
+
+i = 1;
+while (i <= 5) {
+  console.log("8 * " + i + " = " + (8 * i));
+  i++;
+}
+
